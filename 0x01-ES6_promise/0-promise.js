041e35a07558a6fcc9e21fd165214fb40a62bb75
Brianoysters <brianoysters@gmail.com>
2024-07-23 16:21:02 +0000

    promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+function getResponseFromAPI() {
+	return new Promise((resolve, reject) => {
+
+		setTimeout(() => {
+			resolve("API response");
+		}, 1000);
+	});
+}
+
+export default getResponseFromAPI;
+
