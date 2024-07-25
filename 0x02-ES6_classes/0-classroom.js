bb3b067cbc47024d6931270f8c6cee4566dd0e5d
Brianoysters <brianoysters@gmail.com>
2024-07-25 18:56:09 +0000

    holberton

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,8 @@
+export default class ClassRoom {
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,71 @@
+
+export default class HolbertonCourse {
+  /**
+   * Creates a new @see {@link HolbertonCourse}.
+   *
+   * @param {String} name - The name of the course.
+   * @param {Number} length - How long the course is (in months).
+   * @param {String[]} students - The names of students in the course.
+   */
+
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  /**
+   * Gets course name.
+   */
+  get name() {
+    return this._name;
+  }
+
+  /**
+   * Sets name of course.
+   */
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  /**
+   * Gets length of this course.
+   */
+  get length() {
+    return this._length;
+  }
+
+  /**
+   * Sets the length of this course.
+   */
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = value;
+  }
+
+  /**
+   * Gets the names of students.
+   */
+  get students() {
+    return this._students;
+  }
+
+  /**
+   * Sets the names of students.
+   */
+  set students(value) {
+    if (!(value instanceof Array)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    if (!value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = value;
+  }
+}
+
